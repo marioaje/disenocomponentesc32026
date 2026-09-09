2a9655f3665837fe8f232a12e6c33905b68452b4
marioaje <marioaje@gmail.com>
2026-09-08 21:31:30 -0600

    Mi primer componente

---
+++ ComponentesC32026/src/main/java/org/example/Mensaje.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Mensaje {
+    public String mostrarMensaje(String mensajePersonalizado){
+        String mensaje = "Hola," + mensajePersonalizado;
+        return mensaje;
+    }
+
+
+//
+//
+//
+//    Suma
+//
+//            Resta
+//
+//    Multiplicación
+//
+//            División
+//
+//    Raíz cuadrada
+//
+//    Factorial
+
+}
